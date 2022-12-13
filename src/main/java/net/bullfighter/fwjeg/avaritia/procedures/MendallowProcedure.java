6ea6f62e7d2d3f359379f8c56a800eef2d1151e0
Fwjeg <2892354755@qq.com>
2022-12-13 17:47:41 +0800

    Revert "1145"
    This reverts commit cf9e40ebbe060a9644fa1e7368b46d4f1d40240b.

--- src/main/java/net/bullfighter/fwjeg/avaritia/procedures/MendallowProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.bullfighter.fwjeg.avaritia.procedures;
-
-import net.minecraft.world.item.ItemStack;
-
-public class MendallowProcedure {
-	public static void execute(ItemStack itemstack) {
-		(itemstack).shrink(-1);
-	}
-}
--- src/main/java/net/bullfighter/fwjeg/avaritia/procedures/NeverGlowProcedure.java
+++
@@ -1,7 +0,0 @@
-package net.bullfighter.fwjeg.avaritia.procedures;
-
-public class NeverGlowProcedure {
-	public static boolean execute() {
-		return false;
-	}
-}
